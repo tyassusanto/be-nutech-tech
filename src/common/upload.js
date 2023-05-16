f1e4025373478d8ff8d941a920bf6fd5d33a9f8d
Adityas Susanto <92015831+tyassusanto@users.noreply.github.com>
2023-05-16 12:29:01 +0700

    update

--- src/common/upload.js
+++ src/common/upload.js
@@ -1,7 +1,7 @@
 const multer = require('multer');
 
 const storage = multer.diskStorage({
-  destination: process.env.UPLOAD_PATH || './uploads',
+  destination: process.env.UPLOAD_PATH || './upload',
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
     cb(null, file.fieldname + '-' + uniqueSuffix + '-' + file.originalname);
