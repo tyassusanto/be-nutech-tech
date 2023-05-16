188ab5789d0f9e2ce3ecc36be75faa9f617d3503
Adityas Susanto <92015831+tyassusanto@users.noreply.github.com>
2023-05-16 12:26:13 +0700

    update

--- src/routes/product.js
+++ src/routes/product.js
@@ -5,7 +5,7 @@ const upload = require('../common/upload')
 
 route.get('/get', productController.searchProduct) // done
 route.post('/add', productController.addProduct) // done
-route.put('/addimage/:id', upload.single('img'), productController.addProductImage) // done
+route.put('/addimage/:id', upload.single('img'), productController.addProductImage) // done?
 route.delete('/delete/:id', productController.deteleProduct) // done
 route.put('/update/:id', productController.updateProduct) // done
 
